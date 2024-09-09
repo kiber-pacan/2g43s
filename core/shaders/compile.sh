c3940ded929402ce86ad6af8c3f8f43691938139
kiber-pacan <markpustog@gmail.com>
2024-09-09 22:46:47 +0500

    better output

--- core/shaders/compile.sh
+++ core/shaders/compile.sh
@@ -1,2 +1,6 @@
-/home/daunita/Downloads/1.3.283.0/x86_64/bin/glslc shader.vert -o vert.spv
-/home/daunita/Downloads/1.3.283.0/x86_64/bin/glslc shader.frag -o frag.spv
+echo "Compiling shaders"
+
+glslc shader.vert -o vert.spv
+glslc shader.frag -o frag.spv
+
+echo "Done!"
