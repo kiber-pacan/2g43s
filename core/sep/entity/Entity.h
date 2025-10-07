a210f80e71a6274be0dd87ead3a572b5425e5454
kiber-pacan <markpustog@gmail.com>
2025-10-08 00:20:02 +0500

    now more than 2.2x faster

--- core/sep/entity/Entity.h
+++ core/sep/entity/Entity.h
@@ -3,13 +3,15 @@
 #include <utility>
 
 #include "glm/vec3.hpp"
-#include "glm/gtc/quaternion.hpp"
-#include "glm/gtc/matrix_transform.hpp"
+#include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/quaternion.hpp>
+
 
 struct Entity {
     virtual ~Entity() = default;
-
-    std::string name;
 
     glm::mat4 mdlMat{1.0f};
 
@@ -17,19 +19,11 @@ struct Entity {
     explicit Entity(
         const glm::vec3 p = glm::vec3(0.0f),
         const glm::vec3 r = glm::vec3(0.0f),
-        const glm::vec3 s = glm::vec3(1.0f),
-        std::string n = "entity-" + std::to_string(Tools::randomNum<uint32_t>(10000, 99999))
-        ) : name(std::move(n)) {
+        const glm::vec3 s = glm::vec3(1.0f)
+        ) {
         // Pos
         mdlMat = glm::translate(mdlMat, p);
 
-        // Rot
-        mdlMat = glm::rotate(mdlMat, r.x, glm::vec3(1.0f, 0.0f, 0.0f));
-        mdlMat = glm::rotate(mdlMat, r.y, glm::vec3(0.0f, 1.0f, 0.0f));
-        mdlMat = glm::rotate(mdlMat, r.z, glm::vec3(0.0f, 0.0f, 1.0f));
-
-        // Scale
-        mdlMat = glm::scale(mdlMat, s);
     }
 
     virtual void respawn() {
