225a2b9cb072067683a2fb1d01474921ced7066b
kiber-pacan <markpustog@gmail.com>
2025-10-13 00:25:09 +0500

    removed random

--- core/Tools.hpp
+++ core/Tools.hpp
@@ -9,22 +9,6 @@
 // Some tools
 class Tools {
     public:
-
-    // Handy dandy method for easy bounded random
-    template<typename T>
-    static T randomNum(T min, T max) {
-        static std::random_device rd; // максимально случайное семя от системы
-        static std::mt19937_64 rng(rd()); // генератор с большим периодом
-
-        if constexpr (std::is_integral_v<T>) {
-            std::uniform_int_distribution<T> dist(min, max);
-            return dist(rng);
-        } else {
-            std::uniform_real_distribution<T> dist(min, max);
-            return dist(rng);
-        }
-    }
-
     // For reading shader files
     static std::vector<char> readFile(const char* filename) {
         std::ifstream file(filename, std::ios::ate | std::ios::binary);
