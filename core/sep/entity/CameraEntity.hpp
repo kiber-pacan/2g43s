a713835c025dc730208e3d21f6fae719fa0d4bd6
kiber-pacan <markpustog@gmail.com>
2025-10-16 22:18:11 +0500

    refactoring

--- core/sep/entity/CameraEntity.hpp
+++ core/sep/entity/CameraEntity.hpp
@@ -14,9 +14,9 @@ struct CameraEntity {
     glm::vec3 size{};
 
     explicit CameraEntity(
-        glm::vec3 p = glm::vec3(0.0f),
-        glm::vec3 l = glm::vec3(0.0f),
-        glm::vec3 s = glm::vec3(1.0f),
+        const glm::vec3 p = glm::vec3(0.0f),
+        const glm::vec3 l = glm::vec3(0.0f),
+        const glm::vec3 s = glm::vec3(1.0f),
         std::string n = ""
         ) : name(std::move(n)), pos(p), look(l), size(s) {}
 
