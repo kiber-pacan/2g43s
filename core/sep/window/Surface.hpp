102dd54b55705615d81d67aa65ce7b98d2095b02
kiber-pacan <markpustog@gmail.com>
2025-10-16 22:20:19 +0500

    refactoring

--- core/sep/window/Surface.hpp
+++ core/sep/window/Surface.hpp
@@ -5,7 +5,7 @@
 #include <vulkan/vulkan.h>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_vulkan.h>
-#include "../../Logger.hpp"
+#include "../util/Logger.hpp"
 
 struct Surface {
     // Creating surface with SDL3
