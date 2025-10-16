94f0d52a8e0d014aa8058d8f18fb6031f5fb16a2
kiber-pacan <markpustog@gmail.com>
2025-10-16 22:18:27 +0500

    draw indirect

--- core/sep/device/LogDevice.hpp
+++ core/sep/device/LogDevice.hpp
@@ -29,6 +29,7 @@ struct LogDevice {
 
         VkPhysicalDeviceFeatures deviceFeatures{};
         deviceFeatures.samplerAnisotropy = VK_TRUE;
+        deviceFeatures.multiDrawIndirect = VK_TRUE;
 
         VkDeviceCreateInfo createInfo{};
         createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
