d71dacbed030ff93bf7c666d0b1f73b6c6a3d7b7
kiber-pacan <markpustog@gmail.com>
2025-10-17 23:08:55 +0500

    atomoic counter

---
+++ core/sep/buffers/AtomicCounterObject.hpp
@@ -0,0 +1,11 @@
+#ifndef INC_2G43S_ATOMICMBUFFEROBJECT_H
+#define INC_2G43S_ATOMICMBUFFEROBJECT_H
+
+#include <atomic>
+#include <glm/mat4x4.hpp>
+
+struct AtomicCounterObject {
+    uint32_t counter;
+};
+
+#endif //INC_2G43S_ATOMICMBUFFEROBJECT_H
