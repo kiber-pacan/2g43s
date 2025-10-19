bae6f9dd9e57bc7e9bc45852383b6180eb7c2c6d
kiber-pacan <markpustog@gmail.com>
2025-10-19 23:50:05 +0500

    refactoring

--- core/shaders/compile.sh
+++ core/shaders/compile.sh
@@ -3,6 +3,5 @@ echo "Compiling shaders"
 glslc vertex.vert -o vertex.spv
 glslc fragment.frag -o fragment.spv
 glslc matrices.comp -o matrices.spv
-#glslc occlusion.comp -o occlusion.spv
 
 echo "Done!"
