dff677f0c23460e3a640b498af800b444e19c670
kiber-pacan <markpustog@gmail.com>
2025-10-19 23:48:14 +0500

    Culling

---
+++ core/sep/buffers/culling/ModelCullingBufferObject.hpp
@@ -0,0 +1,15 @@
+//
+// Created by down1 on 19.10.2025.
+//
+
+#ifndef INC_2G43S_MODELCULLINGBUFFEROBJECT_H
+#define INC_2G43S_MODELCULLINGBUFFEROBJECT_H
+#include <vector>
+#include <glm/fwd.hpp>
+
+struct ModelCullingBufferObject {
+    std::vector<glm::mat4> models;
+    std::vector<glm::vec4> spheres;
+};
+
+#endif //INC_2G43S_MODELCULLINGBUFFEROBJECT_H
